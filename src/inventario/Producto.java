cba39dcaa188022ed2a016dadc350165ace613f4
MX_G8 <MX_G8@localhost>
2025-03-03 20:12:01 +0100

    2.refactorizacion paquete inventario (apartado2)

--- src/inventario/Producto.java
+++ src/inventario/Producto.java
@@ -1,9 +1,51 @@
 package inventario;
 
 public class Producto {
-    public String n;
-    public double p;
-    public int s;
+
+    /**
+     * @return the n
+     */
+    public String getN() {
+        return n;
+    }
+
+    /**
+     * @param n the n to set
+     */
+    public void setN(String n) {
+        this.n = n;
+    }
+
+    /**
+     * @return the p
+     */
+    public double getP() {
+        return p;
+    }
+
+    /**
+     * @param p the p to set
+     */
+    public void setP(double p) {
+        this.p = p;
+    }
+
+    /**
+     * @return the s
+     */
+    public int getS() {
+        return s;
+    }
+
+    /**
+     * @param s the s to set
+     */
+    public void setS(int s) {
+        this.s = s;
+    }
+    private String n;
+    private double p;
+    private int s;
 
     public Producto(String nombre, double precio, int stock) {
         this.n = nombre;
@@ -12,12 +54,12 @@ public class Producto {
     }
 
     public void mostrarInformacion() {
-        System.out.println("Producto: " + n + " | Precio: " + p + " | Stock: " + s);
+        System.out.println("Producto: " + getN() + " | Precio: " + getP() + " | Stock: " + getS());
     }
 
     public void borrarDatos() {
-        n = "";
-        p = 0.0;
-        s = 0;
+        setN("");
+        setP(0.0);
+        setS(0);
     }
 }
