6d06b1aa8b13b2feefa6991ce575715259a491f3
victor <55408072+victormanuel33333@users.noreply.github.com>
2025-03-05 18:10:21 +0100

    Add files via upload

---
+++ src/Producto.java
@@ -0,0 +1,21 @@
+public class Producto {
+    public String n;
+    public double p;
+    public int s;
+
+    public Producto(String nombre, double precio, int stock) {
+        this.n = nombre;
+        this.p = precio;
+        this.s = stock;
+    }
+
+    public void mostrarInformacion() {
+        System.out.println("Producto: " + n + " | Precio: " + p + " | Stock: " + s);
+    }
+
+    public void borrarDatos() {
+        n = "";
+        p = 0.0;
+        s = 0;
+    }
+}
